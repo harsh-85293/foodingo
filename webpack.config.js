5e0762bcdef68bf472b80b5b4e8b71b629617724
harsh-85293 <harsh.ramchandani122003@gmail.com>
2026-01-14 01:50:10 +0530

    Fix favicon 403 error - add proper favicon.ico file and copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
@@ -40,7 +41,12 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './index.html',
         inject: 'body',
-        favicon: false,
+        favicon: './public/favicon.ico',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: 'public/favicon.ico', to: 'favicon.ico' }
+        ],
       }),
     ],
     resolve: {
